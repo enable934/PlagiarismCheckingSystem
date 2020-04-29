fd02e19691630aeab0ece41f3a44f52ad8c14724
enable934 <black.novak.orest@gmail.com>
2020-04-29 14:10:41 +0300

    set up default port, fix bug

--- PlagiarismCheckingSystem/Program.cs
+++ PlagiarismCheckingSystem/Program.cs
@@ -21,6 +21,7 @@ namespace PlagiarismCheckingSystem
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:5000/");
                 });
     }
 }
